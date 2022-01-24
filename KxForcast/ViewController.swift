2e11f6e795fd39e8a78e73e81191d8ffd139b2cf
k790911 <k790911@gmail.com>
2022-01-24 22:09:25 +0900

    동영상 완강

--- KxForcast/ViewController.swift
+++ KxForcast/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var listTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,3 +20,27 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath)
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: ForcastTableViewCell.identifier, for: indexPath)
+        return cell
+    }
+}
